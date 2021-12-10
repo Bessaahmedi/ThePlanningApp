4f4ff0386998cdcd8cf07476334a42fa0347d57d
Besmira Ahmedi <besmira_ahmedi@hotmail.com>
2021-12-10 02:16:35 +0100

    solved vulnerabilities + test

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+/* Scoped Stylesheet - counts for the header tag ONLY in this component */
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
