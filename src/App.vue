da68f6afb966fa196987049a8e9942bea497f1b9
Besmira Ahmedi <besmira_ahmedi@hotmail.com>
2021-12-18 18:17:13 +0100

    start

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div class="container">
     <Header />
+    <Kalenderikon />
+   
+    
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Kalenderikon from './components/Kalenderikon'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Kalenderikon,
   },
 }
 </script>
 
 <style>
 /* Global Stylesheet - counts for all of the components */
+* {
+  font-family: Arial, Helvetica, sans-serif;
+
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+
+}
+
 .container {
   max-width: 500px;
-  margin: 30px auto;
+  margin: 0 auto;
   overflow: auto;
   min-height: 300px;
-  border: 1px solid steelblue;
-  padding: 30px;
   border-radius: 5px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,32 @@
 <template>
     <header>
-        <h1>Task Tracker</h1>
+        <img :src="img" alt="">
     </header>
 </template>
 
 <script>
+
 export default {
     name: 'Header',
+    data () {
+        return {
+            img : 'assets/saveMyDay.png'
+        }
+    }
 }
 </script>
 
 <style scoped>
 /* Scoped Stylesheet - counts for the header tag ONLY in this component */
+
+
+
 header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin-bottom: 20px;
+    padding: 30px;
+    background-color: pink;
 }
 </style>
